c2f3dff658899cf2b42935b8245277025d5f1fc5
tsuchiya-kai <yukaigangan@gmail.com>
2022-01-16 22:44:20 +0900

    linter周りの設定を完了

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": "error",
+  },
+  ignorePatterns: ["**/blog-database", "**/blog-database-copy"],
+  settings: {
+    React: {
+      version: "detect",
+    },
+  },
+};
